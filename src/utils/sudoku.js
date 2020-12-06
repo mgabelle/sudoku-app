7f19202675c4d0b9bf9bfb67314db7e1480044b6
mgabelle <gabellemathieu18@gmail.com>
2020-12-06 17:14:34 +0100

    start sudoku solver class

--- src/utils/sudoku.js
+++ src/utils/sudoku.js
@@ -57,3 +57,51 @@ const isFull = grid => {
 }
 
 const generateSudokuGrid = () => createEmptyGrid();
+
+//SUDKU SOLVER - BACKTRACKING ALGORITHM
+class SudokuSolver {
+    /*
+        A grid is a 9x9 matrix built with Array.
+        In this class we use : 
+            - i for the line position
+            - j for the column position 
+    */
+    constructor(grid) {
+        this.grid = grid;
+        this.position = [];
+    }
+
+    getGrid = _ => this.grid;
+
+    //Lines
+    getLine = i => this.getGrid()[i];
+
+    isValueInLine = (i, value) => this.isInArray(this.getLine(i), value);
+
+    //Columns
+    getColumn = j => this.getGrid().map(row => row[j]);
+
+    isValueInColumn = (j, value) => this.isInArray(this.getColumn(j), value);
+
+    //Block 
+    getBlock = (i,j) => {
+        const line = Math.floor(i/3);
+        const column = Math.floor(j/3);
+        const block = [];
+        for(let m = line; m < line + 3; m++){
+            for(let n = column; n < column + 3; n++){
+                block.push(this.grid[m][n]);
+            }
+        }
+    }
+
+    isInBlock = (i, j, value) => this.isInArray(this.getBlock(i,j), value);
+
+    //Checker
+    isInArray = (array, value) => {
+        if(value in array) {
+            return true;
+        }
+        return false;
+    }
+}
