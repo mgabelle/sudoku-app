b1b40024ebfdc8adacb6a47a534fa86583832b16
mgabelle <mathieu.gabelle@ericsson.com>
2021-01-01 18:00:31 +0100

    Add sudoku coordinates to global variables

--- src/utils/sudoku-variables.js
+++ src/utils/sudoku-variables.js
@@ -1,2 +1,15 @@
 export const SUDOKU_SIZE = 9;
+
 export const SUDOKU_VALUES = [1, 2, 3, 4, 5, 6, 7 ,8 ,9];
+
+const getSudokuCoordinates = () => {
+    let coordinates = [];
+    for(let i = 0; i < SUDOKU_SIZE; i++){
+        for(let j = 0; j < SUDOKU_SIZE; j++) {
+            coordinates.push([i,j]);
+        }
+    }
+    return coordinates;
+}
+
+export let SUDOKU_COORDINATES = getSudokuCoordinates();
